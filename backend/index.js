d455df9ee4a006ae46dd1d14053800cb8599712b
Yashkhamkar <71758380+Yashkhamkar@users.noreply.github.com>
2023-01-30 21:26:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const path = require("path");
 const connectDB = require("./config/db");
 const app = express();
 dotenv.config();
+mongoose.set("strictQuery", false);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 9002;
