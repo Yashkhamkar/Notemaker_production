df777b200efb08e4662488662097fd1a81e17ec9
Yashkhamkar <71758380+Yashkhamkar@users.noreply.github.com>
2023-01-30 14:35:50 +0530

    code pushed

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+// creating backend in mern
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path");
+const connectDB = require("./config/db");
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 9002;
+app.use(cors());
+connectDB();
+app.use("/api/user", userRoutes);
+app.use("/api/notes", noteRoutes);
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("ggez");
+  });
+}
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
